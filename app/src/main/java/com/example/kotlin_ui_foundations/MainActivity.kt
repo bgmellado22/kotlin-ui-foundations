965d94ada426093335f2b0033dc7ac5d705ab108
Bastihan Gonzalez <bgmellado22@gmail.com>
2026-04-03 16:55:10 -0300

    MainActivity changes

--- app/src/main/java/com/example/kotlin_ui_foundations/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_ui_foundations/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.kotlin_ui_foundations.ui.screens.ComponentsScreen
 import com.example.kotlin_ui_foundations.ui.theme.KotlinuifoundationsTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -21,29 +22,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             KotlinuifoundationsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                ComponentsScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    KotlinuifoundationsTheme {
-        Greeting("Android")
     }
 }
